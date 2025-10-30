086830cbc71af090d2273303048e90cbbf5e2387
jacs121 <101277107+jacs121@users.noreply.github.com>
2025-10-30 19:47:00 +0200

    Bump version to 1.1.0, update dependencies, and modify license in setup.py; remove old distribution files

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="Term-gfx",
     version="1.0.0",
     author="Nitzan Soriano",
+    license="MIT AND (Apache-2.0 OR BSD-2-Clause)",
     author_email="nitzansoriano1@example.com",
     url="https://github.com/jacs121/console-renderer",
     description="A terminal-based graphics library for Python",
@@ -13,7 +14,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
@@ -35,10 +35,5 @@ setup(
             "pytest>=6.0",
             "black",
         ],
-    },
-    entry_points={
-        "console_scripts": [
-            "term-gfx-demo=term_gfx.examples.basic_demo:main",
-        ],
-    },
+    }
 )
